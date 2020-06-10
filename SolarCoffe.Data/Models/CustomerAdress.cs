be1b065837ee501c88a87f7ed00388f0e4270957
Kamii Akbari <kamyab.akbari@hiof.no>
2020-06-11 01:09:07 +0200

    Database PSQL Setup

---
+++ SolarCoffe.Data/Models/CustomerAdress.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SolarCoffe.Data.Models
+{
+    public class CustomerAdress
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        
+        [MaxLength(50)]
+        public string AdressLine1 { get; set; }
+        
+        [MaxLength(50)]
+        public string AdressLine2 { get; set; }
+        
+        [MaxLength(50)]
+        public string City { get; set; }
+        
+        [MaxLength(50)]
+        public string State { get; set; }
+        
+        [MaxLength(20)]
+        public string PostalCode { get; set; }
+        
+        [MaxLength(50)]
+        public string Country { get; set; }
+    }
+}
---
+++ SolarCoffe.Data/SolarDbContex.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SolarCoffe.Data.Models;
+
+namespace SolarCoffe.Data
+{
+    public class SolarDbContex : IdentityDbContext
+    {
+        public SolarDbContex(){}
+        
+        public SolarDbContex(DbContextOptions options) : base(options){}
+        
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<CustomerAdress> CustomerAdresses { get; set; }
+    }
+}
